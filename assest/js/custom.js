99f02cac446b033776b230920fd842a4ab46bedd
janesh <Janesh@codeflies.com>
2025-06-30 09:54:21 +0530

    Commit By Sachin Khanna

--- assest/js/custom.js
+++ assest/js/custom.js
@@ -30,4 +30,36 @@
       };
       reader.readAsDataURL(file);
     }
+
+
+    });
+
+
+
+     // for video
+    jQuery(document).ready(function ($) {
+         $('.play-icon').on('click', function () {
+    var container = $(this).closest('.cb-video-container');
+    var video = container.find('video').get(0);
+
+    if (video) {
+      video.play();
+      container.find('.play-icon').hide();
+      container.find('.pause-icon').show();
+    }
   });
+
+  $('.pause-icon').on('click', function () {
+    var container = $(this).closest('.cb-video-container');
+    var video = container.find('video').get(0);
+
+    if (video) {
+      video.pause();
+      container.find('.pause-icon').hide();
+      container.find('.play-icon').show();
+    }
+  });
+    });
+
+
+
